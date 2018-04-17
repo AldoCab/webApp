f23ca9653e6629f4707e06a59ff3bbfb894d6b71
AldoCab <aldo.dant.13@gmail.com>
2018-04-16 17:25:26 -1200

    Conversion de medidas
    Cm a diferentes medidas

---
+++ conversion/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  operacion1 : string = "";
+  operacion2 : string = "";
+  operacion3 : string = "";
+  operacion4 : string = "";
+
+}
